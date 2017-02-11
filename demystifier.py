ff3817dc8faddc2bff8e3d2fc7340cc0e8eaad6f
dabrygo <dbgodfrey89@msn.com>
2017-02-11 01:09:56 +0000

    Retrieve a random word starting from a given letter

--- demystifier.py
+++ demystifier.py
@@ -10,13 +10,16 @@ def random_element_from(list, seed=None):
     random.seed(seed)
   return random.choice(list)
 
-def word_that_starts_with(letter):
+def word_that_starts_with(letter, seed=0):
   with open('/usr/share/dict/words') as dictionary:
     words = dictionary.readlines()
+    valid_words = []
     for word in words:
       if word.startswith(letter):
-        return word.rstrip() 
-  raise ValueError("{} not found in dictionary".format(letter))
+        valid_words.append(word.rstrip()) 
+  if not valid_words:
+    raise ValueError("{} not found in dictionary".format(letter))
+  return random_element_from(valid_words, seed)
 
 
 class TestDemystifier(unittest.TestCase):
@@ -27,7 +30,7 @@ class TestDemystifier(unittest.TestCase):
     self.assertTrue(letter_in_acronym('i', 'COOPERATION'))
 
   def test_i_starts_with_i(self):
-    self.assertEquals('i', word_that_starts_with('i'))
+    self.assertEquals('intoxicant', word_that_starts_with('i'))
 
   def test_8_not_in_dictionary(self):
     self.assertRaises(ValueError, word_that_starts_with, '8')
